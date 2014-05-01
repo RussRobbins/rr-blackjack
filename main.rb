ef3d41d49b8a89a4168fcf0be4b93942a60dae96
Russ Robbins <russ.robbins@gmail.com>
2014-05-01 07:56:14 -0400

    WIP 2

--- main.rb
+++ main.rb
@@ -4,11 +4,29 @@ require 'sinatra/reloader'
 
 set :sessions, true
 
+get '/restart' do
+  session[:username] = nil
+  redirect '/'
+end
+
 get '/' do
+  if session[:username]
+    redirect '/game'
+  else
+    redirect '/get_name'
+  end
+end
+
+get '/get_name' do
+  session[:cash] = 500
   erb :get_name
 end
 
 post '/get_name' do
-  puts params[:username]
+  session[:username] = params[:username]
+  redirect '/game'
 end
 
+get '/game' do
+  erb :game
+end
